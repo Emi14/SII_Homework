e656eeb754c86af19f71cf390f614f4d7a1104ba
MarArp <marianarpasanu@yahoo.com>
2022-11-22 20:21:40 +0200

    Mar Arp first commit

--- src/main/java/org/siit/homework/week6/Main.java
+++ src/main/java/org/siit/homework/week6/Main.java
@@ -3,6 +3,6 @@ package org.siit.homework.week6;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is the main class for week 6 homework");
-        System.out.println("Mar Arp first commit");
+        System.out.println("Mar Arp first commite");
     }
 }
