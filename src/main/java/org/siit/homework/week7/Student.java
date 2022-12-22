17a28534e225f765ad9633469eab1ef061e4cf69
Razvan Radulescu <48254745+razvan-radulescu@users.noreply.github.com>
2022-12-22 16:09:29 +0200

    tema 7 sapt 7

---
+++ src/main/java/org/siit/homework/week7/Student.java
@@ -0,0 +1,49 @@
+package org.siit.homework.week7;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String gender;
+    private String id;
+
+    public Student(String firstName, String lastName, LocalDate dateOfBirth, String gender, String id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getAge() {
+        LocalDate now = LocalDate.now();
+        Period period = Period.between(dateOfBirth, now);
+        return period.getYears();
+    }
+}
