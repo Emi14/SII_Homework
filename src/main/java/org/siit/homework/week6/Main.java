7b056c6f8bba9dd8534f04371d8d6ffdad56dc70
lucadani7 <lucaionescu1998@gmail.com>
2022-12-23 19:03:40 +0200

    Homework 6 done

--- src/main/java/org/siit/homework/week6/Main.java
+++ src/main/java/org/siit/homework/week6/Main.java
@@ -1,7 +1,15 @@
 package org.siit.homework.week6;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is the main class for week 6 homework");
+        System.out.println();
+        System.out.print("Type your expression: ");
+        Scanner scanner = new Scanner(System.in);
+        String expression = scanner.nextLine();
+        CalculatorInterface calculator = new Calculator();
+        calculator.computeString(expression);
     }
 }
